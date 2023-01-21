1169452003f2d7d929ad8c872f4f1c49431adc97
bazsup <bas@odds.team>
2023-01-21 13:09:33 +0700

    add cloud_pockets db script

--- db/01-init.sql
+++ db/01-init.sql
@@ -5,3 +5,13 @@ CREATE TABLE "accounts" (
     "balance" float8 NOT NULL DEFAULT 0,
     PRIMARY KEY ("id")
 );
+
+CREATE SEQUENCE IF NOT EXISTS cloud_pocket_id;
+
+CREATE TABLE "cloud_pockets" (
+    "id" int4 NOT NULL DEFAULT nextval('cloud_pocket_id'::regclass),
+    "name" VARCHAR(255),
+    "category" VARCHAR(255),
+    "currency" VARCHAR(3),
+    "balance" float8 NOT NULL DEFAULT 0
+)
