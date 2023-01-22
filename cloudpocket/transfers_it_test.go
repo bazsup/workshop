2c4ecc30be5373f032c1a05cab03ef55ecf58c16
bazsup <bas@odds.team>
2023-01-22 16:08:45 +0700

    test invalid decimal amount

--- cloudpocket/transfers_it_test.go
+++ cloudpocket/transfers_it_test.go
@@ -112,3 +112,50 @@ func TestCreateTransferInsufficientAmountShouldBeFail(t *testing.T) {
 	assert.Equal(t, http.StatusBadRequest, rec.Code)
 	assert.JSONEq(t, `{"message":"insufficient balance"}`, string(byteBody))
 }
+
+func TestTransferInvalidDecimalIT(t *testing.T) {
+	e := echo.New()
+
+	cfg := config.New().All()
+	db, err := sql.Open("postgres", cfg.DBConnection)
+	if err != nil {
+		t.Error(err)
+	}
+	row1 := db.QueryRow("INSERT INTO cloud_pockets (name, currency, balance) VALUES ('Travel Fund', 'THB', 50) RETURNING id;")
+	row2 := db.QueryRow("INSERT INTO cloud_pockets (name, currency, balance) VALUES ('Savings', 'THB', 50) RETURNING id;")
+
+	var id1, id2 int
+	row1.Scan(&id1)
+	row2.Scan(&id2)
+
+	hPocket := cloudpocket.New(db)
+
+	e.POST("/transfers", hPocket.Transfer)
+
+	reqBody := fmt.Sprintf(`{"pocket_id_source":%d,"pocket_id_target":%d,"amount":0.001}`, id1, id2)
+	req := httptest.NewRequest(http.MethodPost, "/transfers", strings.NewReader(reqBody))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+
+	rowBalance1 := db.QueryRow("SELECT balance from cloud_pockets WHERE id = $1;", id1)
+	rowBalance2 := db.QueryRow("SELECT balance from cloud_pockets WHERE id = $1;", id2)
+
+	var balance1, balance2 float64
+
+	rowBalance1.Scan(&balance1)
+	rowBalance2.Scan(&balance2)
+
+	byteBody, err2 := io.ReadAll(rec.Body)
+	assert.NoError(t, err2)
+
+	var t2 cloudpocket.Transfer
+	err = json.Unmarshal(byteBody, &t2)
+
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.JSONEq(t, `{"message":"invalid amount"}`, string(byteBody))
+
+	assert.Equal(t, 50.0, balance1)
+	assert.Equal(t, 50.0, balance2)
+}
