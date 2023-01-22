2e8866e2f512d7d27748d95d0fc534a0dad4a588
bazsup <bas@odds.team>
2023-01-22 16:13:23 +0700

    refactor test

--- cloudpocket/transfers_it_test.go
+++ cloudpocket/transfers_it_test.go
@@ -124,31 +124,23 @@ func TestTransferInvalidDecimalIT(t *testing.T) {
 	row1 := db.QueryRow("INSERT INTO cloud_pockets (name, currency, balance) VALUES ('Travel Fund', 'THB', 50) RETURNING id;")
 	row2 := db.QueryRow("INSERT INTO cloud_pockets (name, currency, balance) VALUES ('Savings', 'THB', 50) RETURNING id;")
 
-	var id1, id2 int
-	row1.Scan(&id1)
-	row2.Scan(&id2)
+	var sourceID, targetID int
+	row1.Scan(&sourceID)
+	row2.Scan(&targetID)
 
 	hPocket := cloudpocket.New(db)
 
 	e.POST("/transfers", hPocket.Transfer)
 
-	reqBody := fmt.Sprintf(`{"pocket_id_source":%d,"pocket_id_target":%d,"amount":0.001}`, id1, id2)
+	reqBody := fmt.Sprintf(`{"pocket_id_source":%d,"pocket_id_target":%d,"amount":0.001}`, sourceID, targetID)
 	req := httptest.NewRequest(http.MethodPost, "/transfers", strings.NewReader(reqBody))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
 	e.ServeHTTP(rec, req)
 
-	rowBalance1 := db.QueryRow("SELECT balance from cloud_pockets WHERE id = $1;", id1)
-	rowBalance2 := db.QueryRow("SELECT balance from cloud_pockets WHERE id = $1;", id2)
-
-	var balance1, balance2 float64
-
-	rowBalance1.Scan(&balance1)
-	rowBalance2.Scan(&balance2)
-
-	byteBody, err2 := io.ReadAll(rec.Body)
-	assert.NoError(t, err2)
+	byteBody, err := io.ReadAll(rec.Body)
+	assert.NoError(t, err)
 
 	var t2 cloudpocket.Transfer
 	err = json.Unmarshal(byteBody, &t2)
@@ -156,6 +148,13 @@ func TestTransferInvalidDecimalIT(t *testing.T) {
 	assert.Equal(t, http.StatusBadRequest, rec.Code)
 	assert.JSONEq(t, `{"message":"invalid amount"}`, string(byteBody))
 
-	assert.Equal(t, 50.0, balance1)
-	assert.Equal(t, 50.0, balance2)
+	assert.Equal(t, 50.0, getBalanceFor(db, sourceID))
+	assert.Equal(t, 50.0, getBalanceFor(db, targetID))
+}
+
+func getBalanceFor(db *sql.DB, id1 int) float64 {
+	var rs float64
+	row := db.QueryRow("SELECT balance from cloud_pockets WHERE id = $1;", id1)
+	row.Scan(&rs)
+	return rs
 }
